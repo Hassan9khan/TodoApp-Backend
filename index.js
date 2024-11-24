5ff94bb1ececab95fb5a7308eb05a9e551957ab8
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-11-24 23:27:34 +0500

    files modified

--- index.js
+++ index.js
@@ -136,6 +136,3 @@ app.put("/user/:id", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
